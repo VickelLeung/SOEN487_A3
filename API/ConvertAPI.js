7efd42f00e6461c836be5d500900307281096a21
vickel leung <>
2020-03-11 19:37:23 -0400

    added list currencies api

--- API/ConvertAPI.js
+++ API/ConvertAPI.js
@@ -22,4 +22,16 @@ router.route("/convert_currency").get((req, res) => {
     });
 });
 
+router.route("/list_currencies").get((req, res) => {
+  //convert currency
+  const rp = require("request-promise");
+  rp("https://free.currconv.com/api/v7/currencies?apiKey=" + process.env.apikey)
+    .then(body => {
+      res.send(body);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
 module.exports = router;
