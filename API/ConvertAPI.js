6c8a5222e770e60d5ce514191089d27d3c1f9a73
vickel leung <>
2020-03-11 18:25:12 -0400

    updated backend api

--- API/ConvertAPI.js
+++ API/ConvertAPI.js
@@ -1,33 +1,18 @@
 const router = require("express").Router();
 
 router.route("/convert_currency").get((req, res) => {
-  let amount = req.query.amount;
-  let toCurrency = req.query.toCurr;
-  let fromCurrency = req.query.fromCurr;
-  let convertedAmount = 0;
-
   //convert currency
   const rp = require("request-promise");
   rp(
-    "https://free.currconv.com/api/v7/convert?q=USD_CAD&compact=ultra&apiKey=" +
+    "https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=" +
       process.env.apikey
   )
     .then(body => {
-      console.log(body);
+      res.send(body);
     })
     .catch(err => {
-      console.log(err);
+      res.send(err);
     });
-
-  res.send(
-    "hello world : " +
-      " Amount: " +
-      amount +
-      " toCurren: " +
-      toCurrency +
-      " from: " +
-      fromCurrency
-  );
 });
 
 module.exports = router;
