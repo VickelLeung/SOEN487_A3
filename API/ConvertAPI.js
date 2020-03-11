5329b2b7bf6d3bea77c0af6e2a488dd999da5563
vickel leung <>
2020-03-11 18:55:00 -0400

    fixed backend routes

--- API/ConvertAPI.js
+++ API/ConvertAPI.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 
 router.route("/convert_currency").get((req, res) => {
+  let getFromCurr = req.query.from_currency;
+  let getToCurr = req.query.to_currency;
+
   //convert currency
   const rp = require("request-promise");
   rp(
-    "https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=" +
+    "https://free.currconv.com/api/v7/convert?q=" +
+      getFromCurr +
+      "_" +
+      getToCurr +
+      "&compact=ultra&apiKey=" +
       process.env.apikey
   )
     .then(body => {
