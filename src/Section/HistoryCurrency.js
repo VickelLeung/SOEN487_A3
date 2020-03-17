1549a1df8eaa75bb33ac9990bbc3571cd9dd5567
vickel leung <>
2020-03-17 19:51:12 -0400

    added history charts

--- src/Section/HistoryCurrency.js
+++ src/Section/HistoryCurrency.js
@@ -1,8 +1,31 @@
 import React, { PureComponent } from "react";
 import styled from "styled-components";
+import Axios from "axios";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip
+} from "recharts";
 
 class HistoryCurrency extends PureComponent {
-  componentDidMount = () => {};
+  state = {
+    toDate: new Date(),
+    fromDate: ""
+  };
+
+  componentDidMount = () => {
+    Axios.get(
+      "https://soen487a2backend.herokuapp.com/API/history_currency?toDate=2019-10-10&FromDate=2019-10-09&symbols=CAD"
+    );
+  };
+
+  data = [
+    { name: "Page A", uv: 400 },
+    { name: "Page B", uv: 150 }
+  ];
 
   render() {
     return (
@@ -10,10 +33,34 @@ class HistoryCurrency extends PureComponent {
         <Container>
           <Box>
             <h2>Currency</h2>
+            <LineChart
+              width={600}
+              height={300}
+              data={this.data}
+              margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            >
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+            </LineChart>
           </Box>
 
           <Box>
             <h2>Currency</h2>
+            <LineChart
+              width={600}
+              height={300}
+              data={this.data}
+              margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+            >
+              <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+            </LineChart>
           </Box>
         </Container>
       </div>
