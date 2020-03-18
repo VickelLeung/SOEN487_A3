eda79d4cdd8254f39d0ff21d81d1632b09ebb290
vickel leung <>
2020-03-17 20:04:29 -0400

    fixed endpoint history currency

--- API/HistoryCurrencyAPI.js
+++ API/HistoryCurrencyAPI.js
@@ -1,21 +1,25 @@
 const router = require("express").Router();
 
 router.route("/history_currency").get((req, res) => {
-  let getCurrency = req.query.currency;
-  let toDate = req.query.toDate;
-  let FromDate = req.query.FromDate;
+  let symbols = req.query.symbols;
+  let end_at = req.query.end_at;
+  let start_at = req.query.start_at;
 
-  console.log(toDate);
-  console.log(FromDate);
+  console.log("from " + start_at);
+  console.log("to " + end_at);
 
   //convert currency
   const rp = require("request-promise");
   rp(
-    "https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&symbols=" +
-      getCurrency
+    "https://api.exchangeratesapi.io/history?start_at=" +
+      start_at +
+      "&end_at=" +
+      end_at +
+      "&symbols=" +
+      symbols
   )
     .then(body => {
-      res.send(body);
+      return res.send(body);
     })
     .catch(err => {
       res.send(err);
